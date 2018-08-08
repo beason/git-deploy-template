7e2320ae7957e4b14f0f435d625bc882f6bc6819
Benjamin Eason <benjamin.eason@me.com>
2018-08-08 17:25:33 +0100

    Update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: 'git-deploy-template/',
 
     /**
      * Source Maps
